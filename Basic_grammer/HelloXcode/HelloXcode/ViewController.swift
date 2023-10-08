7b1c6be562a169142ae54bd8e2f41a279c8b3c36
Komorebi_Mac <karan1284@naver.com>
2023-10-08 12:45:39 +0900

    no message

---
+++ Basic_grammer/HelloXcode/HelloXcode/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HelloXcode
+//
+//  Created by Komorebi on 10/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        var a = 10
+        a = 20
+        print(a)
+    }
+
+
+}
+
